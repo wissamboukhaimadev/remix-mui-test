2440e2e5d1ea94aa6fb4f2c49adef1a367be2fa4
wissamboukhaimadev <wissamboukhaima9@gmail.com>
2022-09-12 20:11:08 +0100

    1

--- app/components/HomePageComponents/Home.tsx
+++ app/components/HomePageComponents/Home.tsx
@@ -1,23 +1,32 @@
-import { memo } from 'react';
+import { memo, useRef, useEffect } from 'react';
 import { Box, Button, Heading, Highlight } from '@chakra-ui/react';
 import { useNavigate } from '@remix-run/react'
+import gsap from 'gsap';
 const Home = () => {
 
-  const navigate=useNavigate()
-  
+  const navigate = useNavigate()
 
-  const navigateToPricingPage=()=>{
+
+  const navigateToPricingPage = () => {
     navigate('/pricing')
   }
+  const headingRed = useRef(null)
+  const headingRed2 = useRef(null)
+  useEffect(() => {
+    setTimeout(() => {
+      gsap.from(headingRed.current, { x: 1000, duration: 1, opacity: 0 });
+      gsap.from(headingRed2.current, { y: 1000, duration: 0.8 });
+    }, 100)
+  }, [])
 
   return (
     <Box
       className="px-10 flex justify-center items-center flex-col bg-gray-50"
       style={{ minHeight: 'calc(100vh - 125px)' }}
     >
-      <Heading>We are the best IPTV Provider </Heading>
+      <Heading ref={headingRed}>We are the best IPTV Provider </Heading>
       <div className="py-5" />
-      <Heading className="text-red-400">
+      <Heading ref={headingRed2} className="text-red-400">
         +54,000 IPTV Channels & VODs in 4K & Ultra HD Picture Quality.
       </Heading>
       <div className="py-5" />
@@ -35,9 +44,9 @@ const Home = () => {
       </div>
       <div className="py-10" />
 
-        <Button onClick={navigateToPricingPage} colorScheme={'red'} size="lg">
-          GET STARTED
-        </Button>
+      <Button onClick={navigateToPricingPage} colorScheme={'red'} size="lg">
+        GET STARTED
+      </Button>
     </Box>
   );
 };
